076c1ee75d13319ebb5e6546e922f0c34c82f15b
Maria liz León <marializleonmier.com>
2025-03-07 19:59:35 -0500

    pacial Poo

---
+++ src/MariaLeon/base/Rango.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MariaLeon.base;
+
+import java.util.List;
+
+/**
+ *
+ * @author Maria liz
+ */
+public class Rango {
+     private String nombre;
+    private String descripcion;
+    private String insignia;
+    private String linea;
+    private int tiempoMinAscenso;
+    //private List<CriterioAscenso> criteriosAscenso;
+    //private CuerpoMilitar cuerpoMilitar;
+    private Rango rangoSuperior;
+}
---
+++ src/MariaLeon/base/UnidadMilitar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MariaLeon.base;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Maria liz
+ */
+public class UnidadMilitar {
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private Date fechaCreacion;
+    private String direccion;
+    private String objetivo;
+    private String mision;
+    //private Soldado soldadoDirigente;
+    //private List<UnidadMilitar> subUnidades;
+    
+}
